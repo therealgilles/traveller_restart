723069f643874262033c07caa80445e4d6cbea71
Gilles BOUVIER <gillesbouvier+github@gmail.com>
2018-04-20 23:43:53 -0700

    First commit

--- android/app/src/main/java/com/traveller/MainApplication.java
+++ android/app/src/main/java/com/traveller/MainApplication.java
@@ -3,9 +3,10 @@ package com.traveller;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativeI18n(),
             new VectorIconsPackage(),
-            new RNDeviceInfo()
+            new ReactNativeI18n(),
+            new RNDeviceInfo(),
+            new ReactNativeConfigPackage()
       );
     }
 
--- ios/traveller/AppDelegate.m
+++ ios/traveller/AppDelegate.m
@@ -1,22 +1,26 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "ReactNativeConfig.h"
+@import GoogleMaps;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
+
+  /* Google Maps API key */
+  NSString *googleAPIKey = [ReactNativeConfig envFor:@"GOOGLE_KEY"];
+  [GMSServices provideAPIKey:googleAPIKey];
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
